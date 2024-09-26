f93b4506b45eaa2e71dfae7368576d84f8e612c2
williandK27 <willianfenix5@gmail.com>
2024-09-26 09:24:02 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+//fantasia, Ação, Ficção científica, Comédia, aventura 
+
+// a viagem de chihiro, LIVRE, fantasia, aventura 
+// guardiões da galáxia, 12, fantasia, Ficção científica
+
+// deadpool 2, 18, Comédia, Comédia
+// deadpool 3, 18, Comédi, Comédia
+// galinha pintadinha, LIVRE, fantasia 
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
